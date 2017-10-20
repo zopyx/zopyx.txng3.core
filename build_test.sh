89ae1ca5d1271668f8bb21c9bcf2d29a42c65024
Andreas JungÂ <info@zopyx.com>
2017-10-20 12:54:40 +0200

    uzpdated

--- build_test.sh
+++ build_test.sh
@@ -4,17 +4,7 @@ export PATH=\
 /opt/buildout.python/bin:\
 $PATH:
 
-if [[ "$1" = "python-2.6" ]]
-then
-    python_version=2.6
-fi
-
-if [[ "$1" = "python-2.7" ]]
-then
-    python_version=2.7
-fi
-
-virtualenv-$python_version .
+virtualenv
 bin/python bootstrap.py
 bin/buildout
 make test
